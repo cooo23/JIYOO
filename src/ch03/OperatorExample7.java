0368c53f601dabe4c65a05de2cf0202c88541cdf
jiyoon <cooo23@naver.com>
2021-09-29 13:40:10 +0900

    second

---
+++ src/ch03/OperatorExample7.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public class OperatorExample7 {
+    public static void main(String[] args){
+        int n1 = 10;
+        int n2 = 0;
+
+        System.out.println(n1 / n2);
+        //나누기 할 때 0으로 나누면 에러뜬다.
+    }
+}
